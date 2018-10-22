23fa1fa99798b66175f07663d1f20d54631561fc
pfabrice <wwdev35@gmail.com>
2018-10-22 18:23:24 +0200

    Creation projet + ajout gradle

---
+++ src/main/java/com/imie/gamerpg/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.imie.gamerpg;
+
+/**
+ * @author Fabrice
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
