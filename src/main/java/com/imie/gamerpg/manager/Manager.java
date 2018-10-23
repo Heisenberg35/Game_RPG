581aec8bdd47d90140ca74fe06c60526bce7f028
pfabrice <wwdev35@gmail.com>
2018-10-23 13:47:18 +0200

    Update manager

--- src/main/java/com/imie/gamerpg/manager/Manager.java
+++ src/main/java/com/imie/gamerpg/manager/Manager.java
@@ -5,6 +5,10 @@ package com.imie.gamerpg.manager;
 
 import java.util.ArrayList;
 
+import com.imie.gamerpg.entity.arme.Arme;
+import com.imie.gamerpg.entity.arme.ArmePhysique;
+import com.imie.gamerpg.entity.armure.ArmurePhysique;
+import com.imie.gamerpg.entity.interfaceclass.Barbare;
 import com.imie.gamerpg.entity.personnage.Hero;
 import com.imie.gamerpg.entity.personnage.Monstre;
 
@@ -24,6 +28,12 @@ public class Manager {
 		super();
 		this.heros = new ArrayList<Hero>();
 		this.monstres = new ArrayList<Monstre>();
+	}
+	
+	public void addHero() {
+		Hero hero = new Hero("myOrc", 100, 10, new ArmePhysique("epée", 20, 0, 5), new ArmurePhysique("armureMaille", 0, 10), Barbare);
+		
+		heros.add(hero);
 	}
 	
 	public void init() {
