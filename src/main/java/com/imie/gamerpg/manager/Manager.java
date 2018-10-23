7bca317e710320f012f63553633bd41877ca6939
pfabrice <wwdev35@gmail.com>
2018-10-23 14:05:48 +0200

    Update manager

--- src/main/java/com/imie/gamerpg/manager/Manager.java
+++ src/main/java/com/imie/gamerpg/manager/Manager.java
@@ -6,8 +6,12 @@ package com.imie.gamerpg.manager;
 import java.util.ArrayList;
 
 import com.imie.gamerpg.entity.arme.Arme;
+import com.imie.gamerpg.entity.arme.ArmeMagique;
 import com.imie.gamerpg.entity.arme.ArmePhysique;
+import com.imie.gamerpg.entity.armure.ArmureMagique;
 import com.imie.gamerpg.entity.armure.ArmurePhysique;
+import com.imie.gamerpg.entity.defaultcharacters.DefaultBarbare;
+import com.imie.gamerpg.entity.defaultcharacters.DefaultFighter;
 import com.imie.gamerpg.entity.interfaceclass.Barbare;
 import com.imie.gamerpg.entity.personnage.Hero;
 import com.imie.gamerpg.entity.personnage.Monstre;
@@ -28,12 +32,15 @@ public class Manager {
 		super();
 		this.heros = new ArrayList<Hero>();
 		this.monstres = new ArrayList<Monstre>();
+		addHero();
 	}
 	
 	public void addHero() {
-		Hero hero = new Hero("myOrc", 100, 10, new ArmePhysique("epée", 20, 0, 5), new ArmurePhysique("armureMaille", 0, 10), Barbare);
+		Hero hero = new Hero("leBarbare", 100, 10, new ArmePhysique("epée", 20, 0, 5), new ArmurePhysique("armureMaille", 0, 10), new DefaultFighter());
+		Hero hero2 = new Hero("leMagicien", 100, 10, new ArmeMagique("baguette", 0, 15, 8), new ArmureMagique("armureTissu", 10, 0), new DefaultFighter());
 		
 		heros.add(hero);
+		heros.add(hero2);
 	}
 	
 	public void init() {
