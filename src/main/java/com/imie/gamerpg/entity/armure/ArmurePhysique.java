c1f32352786fcddf97b839fdeaf5d871bbcea4fd
pfabrice <wwdev35@gmail.com>
2018-10-23 12:08:35 +0200

    Mise à jour arme/armure

--- src/main/java/com/imie/gamerpg/entity/armure/ArmurePhysique.java
+++ src/main/java/com/imie/gamerpg/entity/armure/ArmurePhysique.java
@@ -11,6 +11,5 @@ public class ArmurePhysique extends Armure {
 
 	public ArmurePhysique(String nom, int ptsArmureMagique, int ptsArmurePhysique) {
 		super(nom, ptsArmureMagique, ptsArmurePhysique);
-		// TODO Auto-generated constructor stub
 	}
 }
