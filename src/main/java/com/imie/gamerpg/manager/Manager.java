f3d6a2ada08b346930cd4216218f93c6c895b24b
pfabrice <wwdev35@gmail.com>
2018-10-23 13:16:28 +0200

    Création manager

---
+++ src/main/java/com/imie/gamerpg/manager/Manager.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.imie.gamerpg.manager;
+
+import java.util.ArrayList;
+
+import com.imie.gamerpg.entity.personnage.Hero;
+import com.imie.gamerpg.entity.personnage.Monstre;
+
+/**
+ * @author Fabrice
+ *
+ */
+public class Manager {
+	
+	ArrayList<Hero> heros;
+	ArrayList<Monstre> monstres;
+	
+	/**
+	 * Constructeur manager
+	 */
+	public Manager() {
+		super();
+		this.heros = new ArrayList<Hero>();
+		this.monstres = new ArrayList<Monstre>();
+	}
+	
+	
+
+}
