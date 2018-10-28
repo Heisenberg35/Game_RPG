5c88e0cf62b6ac77731cb101bc3c1173e5bf6f59
pfabrice <wwdev35@gmail.com>
2018-10-28 11:26:39 +0100

    Ajout classe Donjon

--- src/main/java/com/imie/gamerpg/Application.java
+++ src/main/java/com/imie/gamerpg/Application.java
@@ -3,6 +3,7 @@
  */
 package com.imie.gamerpg;
 
+
 import com.imie.gamerpg.database.dao.DAOManager;
 import com.imie.gamerpg.entity.arme.Arme;
 import com.imie.gamerpg.manager.Manager;
@@ -21,6 +22,7 @@ public class Application {
 		Manager game_manager = new Manager();
 
 		game_manager.menu();
+		
 	}
 }
 
---
+++ src/main/java/com/imie/gamerpg/entity/donjon/Donjon.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.imie.gamerpg.entity.donjon;
+
+/**
+ * @author Fabrice
+ *
+ */
+public class Donjon {
+
+}
