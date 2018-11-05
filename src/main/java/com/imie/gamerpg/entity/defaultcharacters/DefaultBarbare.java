9af34188969e8921f7a7bf73593198a1d36658be
Romain Matteo <romain.matteo@imie.fr>
2018-11-05 09:26:36 +0100

    Add second weapon to Barbare class

--- src/main/java/com/imie/gamerpg/entity/defaultcharacters/DefaultBarbare.java
+++ src/main/java/com/imie/gamerpg/entity/defaultcharacters/DefaultBarbare.java
@@ -3,6 +3,7 @@
  */
 package com.imie.gamerpg.entity.defaultcharacters;
 
+import com.imie.gamerpg.entity.arme.Arme;
 import com.imie.gamerpg.entity.interfaceclass.Barbare;
 import com.imie.gamerpg.entity.personnage.Personnage;
 
@@ -11,6 +12,16 @@ import com.imie.gamerpg.entity.personnage.Personnage;
  *
  */
 public class DefaultBarbare extends DefaultFighter implements Barbare {
+	
+	private Arme second_weapon;
+
+	public Arme getSecond_weapon() {
+		return second_weapon;
+	}
+
+	public void setSecond_weapon(Arme second_weapon) {
+		this.second_weapon = second_weapon;
+	}
 
 	public DefaultBarbare(Personnage character) {
 		super(character);
